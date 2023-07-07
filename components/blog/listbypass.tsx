e1532d2017be78453223097d9b8ee1bb0a17e3f9
dionannd <dionananda77@gmail.com>
2023-07-07 10:40:35 +0700

    fix(blog): Improve accessibility and add animation to "ListOtherBypass" component

--- components/blog/listbypass.tsx
+++ components/blog/listbypass.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
+import { Transition } from "@headlessui/react";
 import clsx from "clsx";
 import { dataListBypass, dataListOtherBypass } from "constants/listbypass";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 export const ListBypass = () => {
   return (
@@ -30,37 +31,44 @@ export const ListOtherBypass = () => {
   const [isHide, setIsHide] = useState<boolean>(true);
 
   const toggleButton = () => {
-    setIsHide(!isHide);
+    setIsHide((prev) => !prev);
   };
 
   return (
-    <div>
+    <Fragment>
       <button
         onClick={toggleButton}
         className="bg-blue-500 py-2 px-4 rounded-lg"
       >
         {isHide ? "Open Other Apps" : "Hide"}
       </button>
-      <div className={clsx("transition-all duration-100 ml-4")}>
-        {!isHide && (
-          <ul className="space-y-5">
-            {dataListOtherBypass.map((item, index) => (
-              <li key={index} className="">
-                <a
-                  href={item.link}
-                  className=" inline-flex gap-3 items-center m-0 p-0"
-                >
-                  Open&nbsp;
-                  {item.image && (
-                    <img src={item.image} alt="icons" className="m-0 p-0" />
-                  )}
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </div>
+      <Transition
+        as={Fragment}
+        show={!isHide}
+        enter="transform transition duration-[400ms]"
+        enterFrom="opacity-0 translate-y-5 scale-50"
+        enterTo="opacity-100 scale-100"
+        leave="transform duration-200 transition ease-in-out"
+        leaveFrom="opacity-100 scale-100 "
+        leaveTo="opacity-0 scale-95"
+      >
+        <ul className="space-y-5">
+          {dataListOtherBypass.map((item, index) => (
+            <li key={index} className="">
+              <a
+                href={item.link}
+                className=" inline-flex gap-3 items-center m-0 p-0"
+              >
+                Open&nbsp;
+                {item.image && (
+                  <img src={item.image} alt="icons" className="m-0 p-0" />
+                )}
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </Transition>
+    </Fragment>
   );
 };
